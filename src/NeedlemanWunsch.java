2a9a4f7b636f50d9f6f7d4a00381eea82ce8dd00
Marcus Norona <marcusjeremynorona@gmail.com>
2017-11-24 18:01:24 -0800

    first commit

---
+++ src/NeedlemanWunsch.java
@@ -0,0 +1,95 @@
+import java.util.Arrays;	
+public class NeedlemanWunsch {
+	private static String sequence1;
+	private static String sequence2;
+	private static int matchScore;
+	private static int mismatchScore;
+	private static int indelScore;
+	private static String[][] matrix;
+	
+	public NeedlemanWunsch(int matchScore, int mismatchScore, int indelScore) {
+			
+			this.matchScore  = matchScore;
+			this.mismatchScore = mismatchScore;
+			this.indelScore = indelScore;
+			
+	}
+	
+	public static void inputSequences(String sequenceOne, String sequenceTwo) {
+		sequence1 = sequenceOne;
+		sequence2 = sequenceTwo;
+		
+		matrix = new String[(sequenceTwo.length() + 2)][sequenceOne.length() +2];
+		matrix[0][0] = "x";
+		
+		
+		String[] first = sequenceOne.split("");
+		String[] second = sequenceTwo.split("");
+		
+		
+		//put first sequence in the first row of matrix
+		int j = 0;
+		for (int i = 1; i <= first.length; i++) {
+			matrix[0][i] = first[j];
+//			System.out.println(first[j]);
+			
+			j++;
+		}
+		
+		
+		
+		//put second sequence in the first column of the matrix
+		int k = 0;
+		for (int i = 1; i <= second.length; i++){
+//			System.out.println("i: " + i);
+//			System.out.println("k: " + k);
+			matrix[i][0] = second[k];
+//			System.out.println(second[k]);
+			
+			k++;
+		}
+		
+		System.out.println("row length: " +matrix.length);
+		System.out.println("col length: " +matrix[0].length);
+		
+		//initialize row
+		int initIndex = 0;
+		for (int i = 1; i< matrix[0].length; i++){
+			
+			matrix[1][i] = Integer.toString(initIndex);
+//			printMatrix();
+			System.out.println("i: " + i);
+			initIndex += -2;
+		}
+		
+		//initialize column
+		initIndex = 0;
+		for (int i = 1; i< matrix.length; i++){
+			matrix[i][1] = Integer.toString(initIndex);
+			initIndex += -2;
+		}
+		
+		
+		
+		
+	}
+	
+	public static void printMatrix(){
+		for (int i = 0; i < matrix.length; i++){
+			for (int j =0; j < matrix[0].length;j++){
+				System.out.printf("%s \t", matrix[i][j]);
+			}
+			System.out.println();
+		}
+	
+	}
+	
+	public static void main(String[] args){
+		NeedlemanWunsch test = new NeedlemanWunsch(1, -1, -2);
+		test.inputSequences("GAATC", "CATTGA");
+		test.printMatrix();
+	}
+	
+	
+
+}
